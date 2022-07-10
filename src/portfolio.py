a54c34171480db415e422a5439ca641af531e168
yeungadrian <yeungadrianjy@gmail.com>
2022-07-11 00:23:42 +0100

    removing redundant packages

--- src/portfolio.py
+++ src/portfolio.py
@@ -2,7 +2,6 @@ from typing import List, Optional, TypeVar
 
 import numpy as np
 import pandas as pd
-import statsmodels.formula.api as smf
 from pydantic import BaseModel
 
 PandasDataFrame = TypeVar("pandas.core.frame.DataFrame")
